e212521c23ce3b99abaa9d50e6f71a4553c14644
sijiaw96 <109883695+sijiaw96@users.noreply.github.com>
2023-02-22 01:03:27 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: 'Web3WomenUnion'
+  base: 'Web3WomenUnion',
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: "/src" }],
